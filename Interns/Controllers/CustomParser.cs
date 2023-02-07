8a43d7d2bf2f4efa386c6f2d20836a894a62abd7
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2023-02-07 22:02:39 +0100

    Create custom parser.

---
+++ Interns/Controllers/CustomParser.cs
@@ -0,0 +1,28 @@
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Parsing;
+
+namespace Interns.Controllers;
+
+internal static class CustomParser
+{
+    public static Parser GetParser(RootCommand rootCommand)
+    {
+        var parser = new CommandLineBuilder(rootCommand)
+            .UseParseErrorReporting()
+            .UseHelp(ctx =>
+            {
+                ctx.HelpBuilder.CustomizeLayout(
+                    _ =>
+                        HelpBuilder.Default
+                            .GetLayout()
+                            .Skip(15)
+                            .Prepend(
+                                context => context.Output.WriteLine("Error: Invalid command.")
+                            ));
+            })
+            .Build();
+        return parser;
+    }
+}
