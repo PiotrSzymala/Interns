9c86fe4556a7d162c820e5606b9701a47670be8a
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2023-02-07 22:09:29 +0100

    Add class to run app.

---
+++ Interns/AppRunner.cs
@@ -0,0 +1,29 @@
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using Interns.Controllers;
+
+namespace Interns;
+
+internal static class AppRunner
+{
+    public static int Run(string[] args)
+    {
+        var max = Commands.MaxAge();
+        var count = Commands.Count();
+
+
+        max.Handler = CommandHandler.Create<string, IConsole>(MaxAgeCommandHandler.HandleMaxAge);
+        count.Handler = CommandHandler.Create<string, int?, int?, IConsole>(CountCommandHandler.HandleCount);
+
+        var cmd = new RootCommand()
+        {
+            max,
+            count
+        };
+
+        var parser = CustomParser.GetParser(cmd);
+
+        return parser.Invoke(args);
+    }
+}
